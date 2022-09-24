ad256cf1d92a674a75ae74e12b41e1dea3dc3627
nicxzmiller <nicxzmiller2@gmail.com>
2022-09-24 09:34:55 +0200

    Home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Stakelogic</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Games</h1>
+        <div className="games">
+          {data.games.map((game) => (
+            <div className="game" key={game.slug}>
+              <a href={`/game/${game.slug}`}>
+                <img src={game.image} alt={game.name} />
+              </a>
+              <div className="game-info">
+                <a href={`/game/${game.slug}`}>
+                  <p>{game.name}</p>
+                </a>
+                <p>{game.category}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  games: [
+    {
+      name: 'BlackJack',
+      slug: 'American Blackjack',
+      category: 'Card games',
+      image: '/images/bj.jpg',
+    },
+    {
+      name: 'Roulette',
+      slug: 'European Roulettes',
+      category: 'Roulette games',
+      image: '/images/roulette.jpg',
+    },
+    {
+      name: 'Game Shows',
+      slug: 'Game Shows',
+      category: 'Game Shows',
+      image: '/images/gameshow.jpeg',
+    },
+  ],
+};
+
+export default data;
