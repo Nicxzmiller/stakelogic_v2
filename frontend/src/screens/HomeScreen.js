a6202336276726cb092f17e3d0b32f2005aa27c0
nicxzmiller <nicxzmiller2@gmail.com>
2022-09-25 08:17:05 +0200

    fetching data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,23 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/games');
+      setGames(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Games</h1>
       <div className="games">
-        {data.games.map((game) => (
+        {games.map((game) => (
           <div className="game" key={game.slug}>
             <Link to={`/game/${game.slug}`}>
               <img src={game.image} alt={game.name} />
